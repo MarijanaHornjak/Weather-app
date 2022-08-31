38d8a3c9aa539da5f70f9ff77986f2b74bd9ae24
MarijanaHornjak <marijanahornjak95@gmail.com>
2022-08-31 18:05:20 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=06531e5152d4905585f3835510a7dae7`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof data.main !== "undefined"
+          ? data.main.temp > 16
+            ? "app warm"
+            : "app cold"
+          : "app"
+      }
+    >
+      <div className="search">
+        <input
+          type="text"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          placeholder="Search..."
+          onKeyPress={searchLocation}
+        ></input>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+            <p>{data.weather ? <p>{data.sys.country}</p> : null}</p>
+          </div>
+          <div className="temp">
+            {data.main ? (
+              <h1>{data.main.temp.toFixed()}°C</h1>
+            ) : (
+              "Search for City to get informations..."
+            )}
+          </div>
+          <div className="description">
+            <h2>{data.weather ? <p>{data.weather[0].main}</p> : null}</h2>
+            <p>
+              <i>
+                {data.weather ? <p>{data.weather[0].description}</p> : null}
+              </i>
+            </p>
+          </div>
+        </div>
+        {data.main != undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()} °C</p>
+              ) : null}
+
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? (
+                <p className="bold">{data.main.humidity} %</p>
+              ) : null}
+
+              <p>humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
